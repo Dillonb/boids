4f9f5f7d8ed341e4ca9578976bd44e015e0c52aa
Dillon Beliveau <dillonbeliveau@gmail.com>
2024-09-02 19:46:19 -0700

    Add package/app to flake

--- flake.nix
+++ flake.nix
@@ -5,17 +5,37 @@
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
+        shortRev = with self; if sourceInfo?dirtyShortRev then sourceInfo.dirtyShortRev else sourceInfo.shortRev;
         pkgs = (import nixpkgs { inherit system; });
         tools = with pkgs; [
           cmake
           ninja
-          gdb
         ];
         libs = with pkgs; [
           SDL2
         ];
       in
       {
+        packages.default = pkgs.stdenv.mkDerivation
+          {
+            pname = "boids";
+            version = "0.0.1-${shortRev}";
+            src = pkgs.lib.fileset.toSource {
+              root = ./.;
+              fileset = pkgs.lib.fileset.unions [
+                ./main.c
+                ./CMakeLists.txt
+                ./cmake/modules/FindSDL2.cmake
+              ];
+            };
+            buildInputs = libs;
+            nativeBuildInputs = tools;
+            passthru.exePath = "/bin/boids";
+          };
+        apps.default = {
+          type = "app";
+          program = "${self.packages.${system}.default}/bin/boids";
+        };
         devShells.default = pkgs.mkShell {
           buildInputs = tools ++ libs;
           LD_LIBRARY_PATH = "${pkgs.vulkan-loader}/lib";
